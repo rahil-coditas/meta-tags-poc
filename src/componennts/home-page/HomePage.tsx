681cb15e7eeea8e75db2efae9ae2daf20910f380
Rahil Sheikh <rahil.sheikh@coditas.com>
2023-06-12 14:17:37 +0530

    Home page

--- src/componennts/home-page/HomePage.tsx
+++ src/componennts/home-page/HomePage.tsx
@@ -11,18 +11,42 @@ const HomePage = () => {
 	return (
 		<div>
 			<Helmet title="meta-tags">
+				<title>Home Page</title>
+				<meta name="description" content="this is a website" />
+
+				<meta
+					property="og:url"
+					content="https://repo-fork-abdulrahilsheikh.vercel.app/"
+				/>
+				<meta property="og:type" content="website" />
 				<meta property="og:title" content="Home Page" />
 				<meta
-					property="og:site_name"
-					content="localhost:3000/home-page"
-				/>
-				<meta property="og:url" content="localhost:3000/home-page" />
-				<meta
 					property="og:description"
-					content="yo this is home page 🤗"
+					content="this is a Home Page website"
 				/>
-				<meta property="og:type" content="product" />
-				<meta property="og:image" content="" />
+				<meta
+					property="og:image"
+					content="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=755&q=80"
+				/>
+
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta
+					property="twitter:domain"
+					content="repo-fork-abdulrahilsheikh.vercel.app"
+				/>
+				<meta
+					property="twitter:url"
+					content="https://repo-fork-abdulrahilsheikh.vercel.app/"
+				/>
+				<meta name="twitter:title" content="Home Page" />
+				<meta
+					name="twitter:description"
+					content="this is a Home Page website"
+				/>
+				<meta
+					name="twitter:image"
+					content="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=755&q=80"
+				/>
 			</Helmet>
 			<div>HomePage</div>
 			<div>
